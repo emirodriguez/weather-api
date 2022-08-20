a145f761b167f8209e3b450e9da5d4007228159b
emirodriguez <emiliano.rodriguez26@gmail.com>
2022-08-20 17:34:11 -0300

    Move test to another folder

--- tests/Feature/Http/Controller/WeatherControllerTest.php
+++ tests/Feature/Http/Controller/WeatherControllerTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\Feature;
+namespace Tests\Feature\Http\Controller;
 
 use App\Models\Weather;
 use Tests\TestCase;
@@ -16,5 +16,6 @@ class WeatherControllerTest extends TestCase
         $response = $this->get('/api/current?query=' .  $weather->query);
 
         $response->assertStatus(200);
+        $this->assertNotEmpty($response->json('temperature'));
     }
 }
